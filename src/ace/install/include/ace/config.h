921329612acdf43c3bff56a2ec0ee51ac67eb151
wk <kingsalone@sina.com>
2017-06-21 18:34:37 +0800

    fix some bug

--- src/ace/install/include/ace/config.h
+++
@@ -1,5 +0,0 @@
-#ifdef WIN32
-#include "ace/config-win32.h"
-#else
-#include "ace/config-linux.h"
-#endif
--- src/ace/install/share/ace/ace-devel.sh
+++
@@ -1,1 +0,0 @@
-export ACE_ROOT=/home/wangkang/openbras-1.0.0/build/..//src/ace/install/share/ace
